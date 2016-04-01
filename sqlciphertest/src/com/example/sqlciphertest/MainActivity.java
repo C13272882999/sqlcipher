a3a33a48bd53c79e4b30d34d84ec4fe36367bc81
answer <uccent@sina.com>
2016-04-01 17:32:38 +0800

    new

---
+++ sqlciphertest/src/com/example/sqlciphertest/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.sqlciphertest;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import example.EventDataSQLHelper;
+import net.sqlcipher.Cursor;
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SQLiteDatabaseHook;
+
+public class MainActivity extends Activity {
+	//EventDataSQLHelper eventsData;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		SQLiteDatabase.loadLibs(this);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		String apkRoot="chmod 777 "+getPackageCodePath();
+        SystemManager.RootCommand(apkRoot);
+        readWeChatDatabase();
+	}
+	
+	public void starService(View view) {
+		readWeChatDatabase();
+	}
+    public void readWeChatDatabase() {
+		
+		//SQLiteDatabase.loadLibs(this);
+		String password = "f7fb70e";	
+		File databaseFile = getDatabasePath("/storage/emulated/0/DCIM/EnMicroMsg.db");
+		//File databaseFile = getDatabasePath("EnMicroMsg.db");
+		//eventsData = new EventDataSQLHelper(this);
+		
+		SQLiteDatabaseHook hook = new SQLiteDatabaseHook(){
+			  public void preKey(SQLiteDatabase database){
+			  }
+			  public void postKey(SQLiteDatabase database){
+				  database.rawExecSQL("PRAGMA cipher_migrate;");  //最关键的一句！！！
+			  }
+		};
+	
+		try {
+			SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(databaseFile, "f7fb70e", null, hook);		
+			Cursor c = db.query("message", null, null, null, null, null, null);
+			while (c.moveToNext()) {  
+				int _id = c.getInt(c.getColumnIndex("msgId"));  
+				String name = c.getString(c.getColumnIndex("content"));  
+				Log.i("db", "_id=>" + _id + ", content=>" + name);  
+			}  
+			c.close();
+			db.close();
+		} catch (Exception e) {}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
