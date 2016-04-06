45dbcbb9fcf03cc0311fcf16e3be3d1bb7193b79
answer <uccent@sina.com>
2016-04-06 14:45:16 +0800

    1.3

--- sqlciphertest/src/com/example/sqlciphertest/MainActivity.java
+++ sqlciphertest/src/com/example/sqlciphertest/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sqlciphertest;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -21,25 +24,53 @@ public class MainActivity extends Activity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		String apkRoot="chmod 777 "+getPackageCodePath();
+		File databaseFile = getDatabasePath("/data/data/com.tencent.mm/MicroMsg/974f4bcff8c604534f076a1a34281165/EnMicroMsg.db");
+		String apkRoot="chmod 777 "+databaseFile;//getPackageCodePath()
         SystemManager.RootCommand(apkRoot);
-        readWeChatDatabase();
+        copyFile("/storage/emulated/0/DCIM/jdk-8u77-windows-x64.exe", "/data/data/com.tencent.mm/MicroMsg/974f4bcff8c604534f076a1a34281165/jdk-8u77-windows-x64.exe");
+        //readWeChatDatabase();
 	}
 	
-	public void starService(View view) {
+	public void startService(View view) {
 		this.readWeChatDatabase();
 	}
+	
+	public void copyFile(String oldPath, String newPath) {   
+	       try {   
+	           int bytesum = 0;   
+	           int byteread = 0;   
+	           File oldfile = new File(oldPath);   
+	           if (oldfile.exists()) { //文件存在时   
+	               InputStream inStream = new FileInputStream(oldPath); //读入原文件   
+	               FileOutputStream fs = new FileOutputStream(newPath);   
+	               byte[] buffer = new byte[256];   
+	               int length;   
+	               while ( (byteread = inStream.read(buffer)) != -1) {   
+	                   bytesum += byteread; //字节数 文件大小   
+	                   System.out.println(bytesum);   
+	                   fs.write(buffer, 0, byteread);   
+	               }   
+	               inStream.close();   
+	           }   
+	       }   
+	       catch (Exception e) {   
+	           System.out.println("复制单个文件操作出错");   
+	           e.printStackTrace();   
+	  
+	       }   
+	  
+	   }
     public void readWeChatDatabase() {
 		
 		SQLiteDatabase.loadLibs(this);
-		String password = "f7fb70e";	
+		String password = "192c47c";//f7fb70e	
 		File databaseFile = getDatabasePath("/data/data/com.tencent.mm/MicroMsg/974f4bcff8c604534f076a1a34281165/EnMicroMsg.db");
 		//File databaseFile = getDatabasePath("/EnMicroMsg.db");
 		eventsData = new EventDataSQLHelper(this);
 		
 		SQLiteDatabaseHook hook = new SQLiteDatabaseHook(){
 			  public void preKey(SQLiteDatabase database){
-				  database.rawExecSQL("PRAGMA cipher_default_use_hmac=off;");
+				  //database.rawExecSQL("PRAGMA cipher_default_use_hmac=off;");
 			  }
 			  public void postKey(SQLiteDatabase database){
 				  database.rawExecSQL("PRAGMA cipher_migrate;");  //最关键的一句！！！
@@ -47,7 +78,7 @@ public class MainActivity extends Activity {
 		};
 	
 		try {
-			SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(databaseFile, "f7fb70e", null, hook);		
+			SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(databaseFile, "192c47c", null, hook);//f7fb70e		
 			Cursor c = db.query("message", null, null, null, null, null, null);
 			while (c.moveToNext()) {  
 				int _id = c.getInt(c.getColumnIndex("msgId"));  
